bd0ef87266e0d93a56e1f2d9e1ac26d97e450f6f
Saqib Sattar <159882823+saqibsattar07@users.noreply.github.com>
2024-04-04 03:06:50 +0500

    todo-list-app

--- Todo List App/index.js
+++ Todo List App/index.js
@@ -1,8 +1,9 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 let todos = [];
 let condition = true;
-while (condition) {
+async function addTodo() {
     const answers = await inquirer.prompt([
         {
             name: "TODO",
@@ -22,15 +23,93 @@ while (condition) {
         todos.push(TODO);
     }
     else {
-        console.log("Kindly add valid input");
+        console.log(chalk.red("Kindly add valid input"));
     }
 }
-if (todos.length > 0) {
-    console.log("Your Todo List");
-    todos.forEach((todo) => {
-        console.log(todo);
-    });
+function viewTodos() {
+    if (todos.length > 0) {
+        console.log(chalk.blue("Your Todo List"));
+        todos.forEach((todo, index) => {
+            console.log(chalk.green(`${index + 1}. ${todo}`));
+        });
+    }
+    else {
+        console.log(chalk.yellow("No todos found"));
+    }
 }
-else {
-    console.log("No todos found");
+async function updateTodo() {
+    const answers = await inquirer.prompt([
+        {
+            name: "index",
+            type: "number",
+            message: "Enter the index of the todo you want to update:"
+        },
+        {
+            name: "newTodo",
+            type: "input",
+            message: "Enter the new todo:"
+        }
+    ]);
+    const { index, newTodo } = answers;
+    if (todos[index - 1]) {
+        todos[index - 1] = newTodo;
+        console.log(chalk.green("Todo updated successfully!"));
+    }
+    else {
+        console.log(chalk.red("Invalid index."));
+    }
 }
+async function deleteTodo() {
+    const answer = await inquirer.prompt([
+        {
+            name: "index",
+            type: "number",
+            message: "Enter the index of the todo you want to delete:"
+        }
+    ]);
+    const { index } = answer;
+    if (todos[index - 1]) {
+        todos.splice(index - 1, 1);
+        console.log(chalk.green("Todo deleted successfully!"));
+    }
+    else {
+        console.log(chalk.red("Invalid index."));
+    }
+}
+async function viewFunction() {
+    viewTodos();
+}
+async function main() {
+    while (condition) {
+        await addTodo();
+    }
+    if (todos.length > 0) {
+        viewTodos();
+        let action;
+        do {
+            action = await inquirer.prompt({
+                name: "action",
+                type: "list",
+                message: "What do you want to do?",
+                choices: ["View Todos", "Update Todo", "Delete Todo", "Exit"]
+            });
+            switch (action.action) {
+                case "View Todos":
+                    await viewFunction();
+                    break;
+                case "Update Todo":
+                    await updateTodo();
+                    break;
+                case "Delete Todo":
+                    await deleteTodo();
+                    break;
+                case "Exit":
+                    console.log(chalk.blue("Exiting..."));
+                    break;
+                default:
+                    console.log(chalk.red("Invalid action."));
+            }
+        } while (action.action !== "Exit");
+    }
+}
+main();
--- Todo List App/index.ts
+++ Todo List App/index.ts
@@ -1,15 +1,16 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
 
-let todos: string[]= [];
+let todos: string[] = [];
 let condition = true;
 
-while(condition) {
-    const answers : {
+async function addTodo() {
+    const answers: {
         TODO: string,
         addmore: boolean
-    }= await inquirer.prompt([
+    } = await inquirer.prompt([
         {
             name: "TODO",
             type: "input",
@@ -21,22 +22,109 @@ while(condition) {
             message: "Do you want to add more todo?",
             default: false
         }
-    ])
-    const {TODO, addmore} = answers;
-    condition = addmore
-    if(TODO) {
-       todos.push(TODO)
+    ]);
+    const { TODO, addmore } = answers;
+    condition = addmore;
+    if (TODO) {
+        todos.push(TODO);
     } else {
-        console.log("Kindly add valid input");
+        console.log(chalk.red("Kindly add valid input"));
     }
-    
 }
 
-if(todos.length > 0) {
-    console.log("Your Todo List");
-    todos.forEach((todo) => {
-        console.log(todo);
-    })
-} else {
-    console.log("No todos found");
+function viewTodos() {
+    if (todos.length > 0) {
+        console.log(chalk.blue("Your Todo List"));
+        todos.forEach((todo, index) => {
+            console.log(chalk.green(`${index + 1}. ${todo}`));
+        });
+    } else {
+        console.log(chalk.yellow("No todos found"));
+    }
 }
+
+async function updateTodo() {
+    const answers: {
+        index: number,
+        newTodo: string
+    } = await inquirer.prompt([
+        {
+            name: "index",
+            type: "number",
+            message: "Enter the index of the todo you want to update:"
+        },
+        {
+            name: "newTodo",
+            type: "input",
+            message: "Enter the new todo:"
+        }
+    ]);
+    const { index, newTodo } = answers;
+    if (todos[index - 1]) {
+        todos[index - 1] = newTodo;
+        console.log(chalk.green("Todo updated successfully!"));
+    } else {
+        console.log(chalk.red("Invalid index."));
+    }
+}
+
+async function deleteTodo() {
+    const answer: {
+        index: number
+    } = await inquirer.prompt([
+        {
+            name: "index",
+            type: "number",
+            message: "Enter the index of the todo you want to delete:"
+        }
+    ]);
+    const { index } = answer;
+    if (todos[index - 1]) {
+        todos.splice(index - 1, 1);
+        console.log(chalk.green("Todo deleted successfully!"));
+    } else {
+        console.log(chalk.red("Invalid index."));
+    }
+}
+
+async function viewFunction() {
+    viewTodos();
+}
+
+async function main() {
+    while (condition) {
+        await addTodo();
+    }
+
+    if (todos.length > 0) {
+        viewTodos();
+        let action;
+        do {
+            action = await inquirer.prompt({
+                name: "action",
+                type: "list",
+                message: "What do you want to do?",
+                choices: ["View Todos", "Update Todo", "Delete Todo", "Exit"]
+            });
+
+            switch(action.action) {
+                case "View Todos":
+                    await viewFunction();
+                    break;
+                case "Update Todo":
+                    await updateTodo();
+                    break;
+                case "Delete Todo":
+                    await deleteTodo();
+                    break;
+                case "Exit":
+                    console.log(chalk.blue("Exiting..."));
+                    break;
+                default:
+                    console.log(chalk.red("Invalid action."));
+            }
+        } while(action.action !== "Exit");
+    }
+}
+
+main();
